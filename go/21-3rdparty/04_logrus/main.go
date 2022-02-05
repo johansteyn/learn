50f9f95a7da903993a9f6f552078f509cdb64493
Johan Steyn <johan.steyn@gmail.com>
2022-02-05 20:46:23 +0000

    WIP

---
+++ go/21-3rdparty/04_logrus/main.go
@@ -0,0 +1,10 @@
+package main
+
+import log "github.com/sirupsen/logrus"
+
+func main() {
+	log.Info("Information about flight departures and arrivals")
+	log.Warn("Bill Stickers will be prosecuted")
+	log.Fatal("Watch out for the big pffft... What pfft?")
+}
+
