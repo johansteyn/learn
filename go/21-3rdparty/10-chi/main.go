9e35119af3d5b4e908a74c7462f72339116706f5
Johan Steyn <johan.steyn@acronis.com>
2023-08-28 11:33:26 +0200

    WIP

--- go/21-3rdparty/10-chi/main.go
+++ go/21-3rdparty/10-chi/main.go
@@ -10,16 +10,29 @@ import (
 
 func main() {
 	router := chi.NewRouter()
+	// Using this before the logger inserts a unique ID for each request
+	router.Use(middleware.RequestID)
 	router.Use(middleware.Logger)
+	// A recoverer handles panics by logging the panic (with backtrace) and returning 500 (Internal Server Error) response.
+	router.Use(middleware.Recoverer)
 	router.Get("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Root path"))
+		rw.Write([]byte("root"))
 	})
 	router.Get("/helloworld", HelloWorldHandler)
+	router.Get("/panic", PanicHandler)
 	http.ListenAndServe(":80", router)
 }
 
 func HelloWorldHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Println("*** HelloWorldHandler")
+	fmt.Printf("=== Remote Address: %v\n", r.RemoteAddr)
 	rw.WriteHeader(http.StatusOK)
 	fmt.Fprintln(rw, "Hello World!")
+}
+
+func PanicHandler(rw http.ResponseWriter, r *http.Request) {
+	var zero = 0
+	fmt.Printf("=== Divide by zero: %d\n", 1/zero)
+	// This statement is not reached...
+	rw.Write([]byte("PANIC!"))
 }
