604e039fa7347e70d7e49df498462f49415aab0f
Johan Steyn <johan.steyn@acronis.com>
2024-05-23 17:07:56 +0200

    WIP

--- go/20_stdlib/01_os/main.go
+++ go/20_stdlib/01_os/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -20,5 +24,49 @@ func main() {
 	fmt.Printf("os.Stdout.Stat(): %s\n", fileInfo)
 	fmt.Printf("Mode: %s\n", fileInfo.Mode())
 	fmt.Printf("os.ModeCharDevice: %s\n", os.ModeCharDevice)
+
+	// Allow graceful shutdown when the user presses Ctrl+C
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	count = 0
+	go func() {
+		<-c
+		fmt.Printf("Received signal while doing job #%d...\n", count)
+		fmt.Println("Calling shutdown...")
+		shutdown()
+	}()
+	for count = 1; count <= 10; count++ {
+		if shutdownRequested {
+			fmt.Println("Breaking...")
+			break
+		}
+		fmt.Printf("Starting job #%d...\n", count)
+		work(count)
+	}
+	fmt.Println("Sleeping for 4 seconds...")
+	time.Sleep(4 * time.Second)
+	fmt.Println("Completed without interruption.")
 }
 
+var count int
+var wg sync.WaitGroup
+
+var shutdownRequested bool
+
+func work(i int) {
+	wg.Add(1)
+	defer wg.Done()
+	fmt.Printf("[#%d]Working...\n", i)
+	time.Sleep(4 * time.Second)
+	fmt.Printf("[#%d]Done.\n", i)
+}
+
+func shutdown() {
+	shutdownRequested = true
+	fmt.Printf("Shutdown, waiting for job #%d to finish...\n", count)
+	wg.Wait()
+	// No guarantee that anything will be called after waiting.
+	// But the 4-second sleep in main gives enough time...
+	fmt.Println("Exiting...")
+	os.Exit(0)
+}
