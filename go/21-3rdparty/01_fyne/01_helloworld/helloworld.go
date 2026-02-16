95830a2c30419877f98e1004ffd49769315818ee
Johan Steyn <johan.steyn@gmail.com>
2026-02-16 09:30:13 +0200

    Updated 'fyne' experiment

--- go/21-3rdparty/01_fyne/01_helloworld/helloworld.go
+++ go/21-3rdparty/01_fyne/01_helloworld/helloworld.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-  "fyne.io/fyne/app"
-  "fyne.io/fyne/widget"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-  a := app.New()
-  window := a.NewWindow("Hello World")
-  window.SetContent(widget.NewLabel("Hello World!"))
-  window.ShowAndRun()
+	a := app.New()
+	window := a.NewWindow("Hello World")
+	window.SetContent(widget.NewLabel("Hello World!"))
+	window.ShowAndRun()
 }
-
